9feacd63d447c9f451251c08a3b29948baf3daa2
huanzhiqiang <huanzhiqiang@jiangduoduo.com>
2021-03-08 20:07:40 +0800

    change README.md

--- examples/server.js
+++ examples/server.js
@@ -8,6 +8,20 @@ const WebpackConfig = require('./webpack.config')
 const app = express()
 const compiler = webpack(WebpackConfig)
 
+app.use(webpackDevMiddleware(compiler, {
+  publicPath: '/__build__/',
+  stats: {
+    colors: true,
+    chunks: false
+  }
+}))
+
+app.use(webpackHotMiddleware(compiler))
+
+app.use(express.static(__dirname))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const router = express.Router()
 
@@ -31,21 +45,6 @@ router.post('/base/buffer', function(req, res) {
 
 app.use(router)
 
-
-app.use(webpackDevMiddleware(compiler, {
-  publicPath: '/__build__/',
-  stats: {
-    colors: true,
-    chunks: false
-  }
-}))
-
-app.use(webpackHotMiddleware(compiler))
-
-app.use(express.static(__dirname))
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
 
 const port = process.env.PORT || 8080
 module.exports = app.listen(port, () => {
