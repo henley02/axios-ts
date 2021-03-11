67181a83d8751bff53418efc6bdb3f86f2112e43
huanzhiqiang <huanzhiqiang@jiangduoduo.com>
2021-03-11 14:45:39 +0800

    feat: add interceptor

--- examples/server.js
+++ examples/server.js
@@ -30,6 +30,7 @@ registerSimpleRouter()
 registerBaseRouter()
 registerErrorRouter()
 registerExtendRouter()
+registerInterceptorRouter();
 
 app.use(router)
 
@@ -121,5 +122,11 @@ function registerExtendRouter() {
         age: 123
       }
     })
+  })
+}
+
+function registerInterceptorRouter() {
+  router.get('/interceptor/get', function(req, res) {
+    res.end('hello')
   })
 }
